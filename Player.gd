b2580b7c1fe0b494d7657c98a13d6c5cdfccf6b2
Yuan Sambo <yuan.sambo@gmail.com>
2023-10-07 17:06:31 +0800

    Add collision detector

--- Player.gd
+++ Player.gd
@@ -13,13 +13,13 @@ extends CharacterBody2D
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	
-func get_input() -> void:
+func _get_input() -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 
 func _process(delta: float) -> void:
-	get_input()
+	_get_input()
 		
 	position.x = clamp(position.x,bounds_offset,screen_size.x-bounds_offset)
 	position.y = clamp(position.y,bounds_offset,screen_size.y-bounds_offset)
@@ -41,5 +41,6 @@ func shoot() -> void:
 	get_tree().root.add_child(projectile)
 	projectile.start(position + Vector2(0, -10))
 
-	
-	
+# Detect collision
+func _on_enemy_detector_area_entered(area):
+	queue_free()

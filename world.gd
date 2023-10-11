11501d85a2d29bd4503c8bbfe81d019961aa6325
Yuan Sambo <yuan.sambo@gmail.com>
2023-10-11 22:06:44 +0800

    Initialize player health for health_ui

--- world.gd
+++ world.gd
@@ -1,12 +1,17 @@
 extends Node
 
-@onready var health_ui = $CanvasLayer/HealthUI
+@export var player : Player
+@export var health_ui : Label
 
 func _ready():
 	# Set starting healthUI value
+	setup_ui()
 	Events.player_health_updated.connect(_update_health_ui)
-
+	
+func setup_ui():
+	var player_health = player.health_component.health
+	_update_health_ui(player_health)
+	
 func _update_health_ui(health : float ):
-	print("Health UI Updated")
 	health_ui.text = str(health)
 	
